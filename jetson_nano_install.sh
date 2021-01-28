0090599578d261f02515bd7d274933bdb388f07d
Łukasz Wielgosz <lukasz.wielgosz9@gmail.com>
2021-01-28 11:09:30 +0100

    Update jetson_nano_install.sh
    fixed issue where with initrd.img not existing

--- jetson_nano_install.sh
+++ jetson_nano_install.sh
@@ -1,4 +1,4 @@
-update-initramfs -c -k `uname -r`
+
 
 # Change a few things in /etc/initramfs-tools so that overlayfs is used on startup to mount a ready-only root
 echo overlay >> /etc/initramfs-tools/modules
@@ -10,7 +10,10 @@ chmod +x /etc/initramfs-tools/hooks/root-ro
 chmod +x /etc/initramfs-tools/scripts/init-bottom/root-ro
 
 # Update the initrd file
-update-initramfs -u
+#update-initramfs -u
+
+# Generate new initrd file
+update-initramfs -c -k `uname -r`
 
 # Now, the file /boot/initrd.img-4.9.140-tegra contains the above modifications.
 # There is also the symbolic link in /boot/initrd.img pointing to it, but this doesn't
@@ -18,6 +21,9 @@ update-initramfs -u
 # So change /boot/extlinux/extlinux.conf so that it actually gets loaded:
 # Search for `INITRD /boot/initrd` and replace it with `INITRD /boot/initrd.img`:
 if ! grep -q "INITRD /boot/initrd.img" /boot/extlinux/extlinux.conf; then sed -i "s/INITRD \/boot\/initrd/INITRD \/boot\/initrd.img/" /boot/extlinux/extlinux.conf; fi
+
+rm /boot/initrd.img
+ln -rs /boot/initrd.img-`uname -r` initrd.img
 
 # Before rebooting, also add the helper scripts from JasperE84/root-ro:
 #wget https://raw.githubusercontent.com/JasperE84/root-ro/master/reboot-to-readonly-mode.sh -O /root/reboot-ro
